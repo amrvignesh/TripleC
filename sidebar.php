b20251663d42365d8e8f250d66e7c30d6dba6810
liginarul <ligin@cairocorps.com>
2017-03-10 11:02:35 +0530

    Added sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,26 @@
+
+<div id="sidebar">
+<div id="side-sidebar">
+<?php
+if(is_active_sidebar('side-sidebar')){
+dynamic_sidebar('side-sidebar');
+}
+?>
+</div>
+<div id="side-sidebar1">
+<?php
+if(is_active_sidebar('side-sidebar-1')){
+dynamic_sidebar('side-sidebar-1');
+}
+?>
+</div>
+<div id="side-sidebar2">
+<?php
+if(is_active_sidebar('side-sidebar-2')){
+dynamic_sidebar('side-sidebar-2');
+}
+?>
+</div>
+
+
+</div>
