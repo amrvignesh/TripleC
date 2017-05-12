9ea63c2f302e6a2c1f42327d83d976890a1bc471
liginarul <ligin@cairocorps.com>
2017-05-12 14:43:09 +0530

    added footer.php

--- footer.php
+++ footer.php
@@ -26,25 +26,8 @@ dynamic_sidebar('footer-sidebar-3');
 </div>
 </div>
 <?php echo wp_footer(); ?>
-<?php echo comicpress_copyright(); ?>
+<?php echo triplec_comicpress_copyright(); ?>
 
-<script type="text/javascript">
-	$('body').prepend('<a href="#" class="back-to-top">Back to Top</a>');
-	var amountScrolled = 300;
 
-$(window).scroll(function() {
-	if ( $(window).scrollTop() > amountScrolled ) {
-		$('a.back-to-top').fadeIn('slow');
-	} else {
-		$('a.back-to-top').fadeOut('slow');
-	}
-});
-$('a.back-to-top').click(function() {
-	$('html, body').animate({
-		scrollTop: 0
-	}, 700);
-	return false;
-});
-</script>
 </body>
 </html>
